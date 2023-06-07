e6ba3958c8bb77d8488f268054ca4c86448c45f5
Erick <erickkirui653@gmail.com>
2023-06-07 15:37:47 -0400

    updated functions and models table arguments

--- models/customer_operations.py
+++ models/customer_operations.py
@@ -1,24 +1,63 @@
 from datetime import datetime
 from dbconnection import session
 from models import Customer, Transaction
+from datetime import datetime
 
+ 
 def add_customer():
-    name = input("Enter customer name: ")
-    customer = Customer(name=name)
-    session.add(customer)
-    session.commit()
-    print("Customer added successfully!")
-
+    # customer should not be able to enter null values
+    # if customer exist should not be able to added twice
+     
+     name = input("Enter customer name: ")
+     phone_number = input("Enter customer phone: ")
+     email = input("Enter customer email: ")
+     
+ 
+     customer = Customer(name=name, phone_number=phone_number, email=email)
+     session.add(customer)
+     session.commit()
+     print("Customer added successfully!")
+ 
+ 
 def add_transaction():
+    # culculate point base on amount 
+    #colum return  customer name instead of id 
+     customer_name = input("Enter customer name: ")
+     amount = input("Enter transaction amount: ")
+     transaction_date = datetime.now()
+ 
+     customer = session.query(Customer).filter(Customer.name == customer_name).first()
+     if customer:
+         transaction = Transaction(amount=amount, transaction_date=transaction_date, customer=customer)
+         session.add(transaction)
+         session.commit()
+         print("Transaction added successfully!")
+     else:
+         print("Customer not found!")
+ 
+ 
+ 
+ ##########################
+ def update_point_balance():
     user_id = input("User ID: ")
-    amount = int(input("Transaction Amount: "))
-    transaction_date = datetime.now()
+    user = session.query(User).filter(User.id == user_id).first()
 
-    customer = session.query(Customer).filter(Customer.id == user_id).first()
-    if customer:
-        transaction = Transaction(amount=amount, datetime=transaction_date, customer=customer)
-        session.add(transaction)
-        session.commit()
-        print("Transaction added successfully!")
-    else:
-        print("Customer not found!")
+    if user:
+        point_balance = session.query(PointBalance).filter(PointBalance.user_id == user_id).first()
+        if point_balance:
+            # Calculate the total points earned by the user
+            total_points = sum(transaction.amount // 10 for transaction in user.transactions)
+            point_balance.total_points = total_points
+        else:
+            
+    
+ 
+ 
+ ##########################
+
+#add = add_customer()
+add2 = add_transaction()
+ 
+ 
+print(add2)
+#print(add2)
--- models/models.py
+++ models/models.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append(os.getcwd)
 
-from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String,Integer, ForeignKey, DateTime)
+from sqlalchemy import (create_engine, PrimaryKeyConstraint,UniqueConstraint,CheckConstraint, Column, String,Integer, ForeignKey, DateTime)
 # from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
@@ -15,9 +15,23 @@ engine = create_engine('sqlite:///database/loyalty.db', echo=True)
 
 class Customer(Base):
     __tablename__ = 'customers'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        
+        UniqueConstraint(
+            'email','name',
+            name='uix_1'),
+        
+
+    )
+    
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    phone_number = Column(Integer)
+    email = Column(String)
     transactions = relationship("Transaction", back_populates="customer")
 
 
@@ -39,8 +53,8 @@ class Loyaltypoints(Base):
     point = Column(Integer)
 
 
-
-
+#delete tables
+#Base.metadata.drop_all(engine)
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
